321cb34f3a6732381c5160e5149baacd87135cf0
jason23347 <18945107883@163.com>
2019-03-23 16:49:05 +0800

    init

--- include/debug.h
+++ include/debug.h
@@ -24,6 +24,7 @@ __debug (FILE *__restrict__ __stream, const char *__fmt, ...)
 
 #define debug(fmt, ...)
 #define debug_r(file, fmt, ...)
+
 #endif
 
 #endif
--- include/log.h
+++ include/log.h
@@ -2,6 +2,7 @@
 #define LOG_H
 
 #include <stdio.h>
+#include <time.h>
 
 FILE *log_stream;
 
